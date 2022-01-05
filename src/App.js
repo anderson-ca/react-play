8a77ad2e174b532e77c9571afb91484d427c4690
anderson <anderson.d.cardoso92@gmail.com>
2022-01-05 11:39:28 -0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavComponent from './components/NavComponent/NavComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <NavComponent />
     </div>
   );
 }
---
+++ src/components/NavComponent/NavComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavComponent = () => {
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <a href="/" class="brand-logo">Playground</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Home</a></li>
+                    <li><a href="badges.html">About</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavComponent
